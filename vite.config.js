1da4b12af25e6d9fb7ba2fe3ce03eb9de7ed9874
shyns2108 <quynhanh21082k1@gmail.com>
2022-04-06 13:15:22 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,14 @@ export default defineConfig({
   },
   server: {
     port: 8081,
+     proxy: {
+       "/api": {
+       target: "http://localhost:8080/",
+       changeOrigin: true,
+       },
+       }
+      
     },
+    
 })
 
